bcc9e6ae897a62ed6e188d0bcc05b6f36c28d07d
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-06-24 18:50:02 -0400

    funcionalidad agregada a los componentes.

--- app/src/main/java/cl/awakelab/hellotoast/MainActivity.java
+++ app/src/main/java/cl/awakelab/hellotoast/MainActivity.java
@@ -3,6 +3,10 @@ package cl.awakelab.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intitListener();
+
+    }
+
+    private void intitListener() {
+
+        Button btnToast = findViewById(R.id.toatsButton);
+        Button btnContador = findViewById(R.id.countButton);
+        TextView textContador = findViewById(R.id.countTextView);
+        
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Hello Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnContador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer numero = Integer.parseInt(textContador.getText().toString()) ;
+                numero++;
+                textContador.setText(numero.toString());
+            }
+        });
+
+       
     }
 }
